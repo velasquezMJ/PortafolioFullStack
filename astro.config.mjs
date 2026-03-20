3fd869ac2b4f1465a8978f3f261f87acedd5c8a9
= <=>
2026-03-20 11:29:13 -0600

    Cambio a release

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://velasquezmj.github.io', // Tu URL de GitHub Pages
-  base: '/Portafolio', // El nombre de tu repositorio en GitHub
+  base: '/PortafolioMvelasquez', // El nombre de tu repositorio en GitHub
   vite: {
     plugins: [tailwindcss()],
   },
